080b7f67ae87530794689b7eabb190bd414efa42
h-inouchi <hibird2720804@gmail.com>
2019-01-20 23:11:45 +0900

    kadai

---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.profile')
+@section('title', 'My プロフィール')
+
+@section('content')
+  <h1>Myプロフィール編集画面</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,14 @@
 */
 Route::get('/', 'NewsController@index');
 
-Route::group(['prefix' => 'admin'], function() {
-  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-  Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
-  Route::get('news', 'Admin\NewsController@index')->middleware('auth');
-  Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
-  Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
-  Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+  Route::get('news/create', 'Admin\NewsController@add');
+  Route::post('news/create', 'Admin\NewsController@create');
+  Route::get('news', 'Admin\NewsController@index');
+  Route::get('news/edit', 'Admin\NewsController@edit');
+  Route::post('news/edit', 'Admin\NewsController@update');
+  Route::get('news/delete', 'Admin\NewsController@delete');
+  Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
 Auth::routes();
